1b524c1a13d1ca64f6094148b6c501c438f4807d
Janet Lee <janetjhu96@gmail.com>
2017-09-25 18:12:05 -0700

    Add headline, fix sidebar layout

--- data_generator.js
+++ data_generator.js
@@ -32,9 +32,9 @@ var randomElement = function(array){
 
 // random tweet generator
 var opening = ['just', 'DUDE', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'cheetoface', 'that wizard', 'a ninja', 'a bag of doritos'];
-var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
+var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'trashed', 'experienced', 'navigated', 'aided', 'enjoyed', 'sold', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of', 'a bag of doritos'];
-var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'grumloxes', 'money', 'way of life', 'belief system', 'nail clippings', 'bad decision', 'future', 'life', 'pony', 'mind'];
 var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '#lol', '', '', ''];
 
 var randomMessage = function(){
@@ -56,10 +56,76 @@ for(var i = 0; i < 20; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 150);
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
 scheduleNextTweet();
 
+function fetchTweets(){
+
+  var $mainBody = $('.main');
+  $mainBody.html('');
+
+  var $refreshButton = $('<span class="button" id="button" style="background: linear-gradient(rgb(255, 196, 119) 5%, rgb(251, 158, 37) 100%) rgb(255, 196, 119); border-radius: 13px; border: 2px solid rgb(238, 180, 79); display: inline-block; cursor: pointer; color: rgb(255, 255, 255); font-family: Arial; font-size: 15px; font-weight: bold; padding: 1px 31px; text-decoration: none; margin: 10px, align: center;">Refresh Tweets!</span>');
+
+  $refreshButton.appendTo($mainBody);
+
+  $(".button").click(function(){
+    fetchTweets();
+  })
+
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div></div>');
+    var $tweetTime = $('<div></div>');
+    var $tweetUser = $('<div id="user"></div>');
+    var $tweetRelativeTime = $('<div></div>');
+    var timeDff = Date.now() - tweet.created_at;
+
+    $tweetUser.text('@' + tweet.user);
+    $tweetUser.css({'font-size': '1.5em', 'font-style': 'bolder', 'padding-top': '20px', 'padding-bottom': '0px'});
+
+    $tweet.text(tweet.message);
+    $tweet.css({'font-size': '2em', 'font-style': 'bolder', 'padding-top': '0px', 'padding-bottom': '0px'});
+
+    $tweetTime.text('Tweeted at: ' + tweet.created_at.toLocaleString());
+    $tweetTime.css({'font-style': 'italic', 'padding-top': '10px'});
+
+    $tweetUser.appendTo($mainBody);
+    $tweet.appendTo($mainBody);
+    $tweetTime.appendTo($mainBody);
+
+    index -= 1;
+  }
+}
+
+function fetchUserTweets(user){
+  var $sidebarRight = $('.sidebar-right');
+  $sidebarRight.html('').css({'background-color': '#9BBBCA'});
+
+  var tweets = userTweets(user);
+  var index = tweets.length - 1;
+  while (index >= 0){
+    var tweet = tweets[index];
+    var $tweet = $('<div></div>');
+    var $tweetTime = $('<div></div>');
+    var $tweetRelativeTime = $('<div></div>');
+    var timeDff = parseInt((Date.now() - tweet.created_at)/1000);
+
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+
+    $tweetTime.text('Tweeted ' + timeDff + ' seconds ago');
+    $tweetTime.css({'font-size': '0.75em', 'font-style': 'italic', 'padding-top': '0px', 'padding-bottom': '20px'});
+
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+
+    $tweet.appendTo($sidebarRight);
+    $tweetTime.appendTo($sidebarRight);
+
+    index-= 1;
+
+  }
+}
 
 var userTweets = function(user) {
   return streams.users[user].splice(-10);
